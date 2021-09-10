53b8e8615123e2629c7a39950111f76e0f15edf7
dtor068 <dtor068@aucklanduni.ac.nz>
2021-09-10 16:26:56 +1200

    Removed 2nd temperature benchmark

--- benchmark.py
+++ benchmark.py
@@ -76,7 +76,7 @@ def pressure_benchmark():
     plt.show()
 
 
-def temperature_benchmark1():
+def temperature_benchmark():
     ''' Check if the temperature fit the model in the first scenario of the solver
 
         Parameter:
@@ -151,82 +151,6 @@ def temperature_benchmark1():
     plt.show()
 
 
-def temperature_benchmark2():
-    ''' Check if the temperature fit the model in the second scenario of the solver
-
-        Parameter:
-        ---------
-        None
-
-        Return:
-        -------
-        None
-    '''
-    # Initialise parameters for the ode model
-    p0 = 200
-    a1 = 2
-    b = 1
-    t0 = 0
-    t1 = 15
-    dt = 1/3
-    bt = 0.5
-    T0 = 25
-    M0 = 100
-
-    nt = int(np.ceil((t1-t0)/dt))  # Compute number of Euler steps to take
-    t = t0+np.arange(nt+1)*dt
-    q1 = 0*t
-    q1.fill(10)
-    P = 15*q1
-    # The ode model is dT/dt = 72.065 - 1.35T
-    pars2 = [a1, b, bt, p0, T0, M0]
-    # Solve the ode numerically
-    ts, xsn = solve_ode_temp(temp_ode_model, t0, t1, dt, q1, T0, P, pars2)
-    # Solve the ode with the given parameter analytically with Wolfram Alpha
-    xsa = -28.3815*np.exp(-1.35*t)+53.3815
-
-    # Creating plot figure and axes
-    plt.rcParams["figure.figsize"] = (15, 5)
-    _, (ax1, ax2, ax3) = plt.subplots(1, 3)
-
-    # Plotting the graph for comparing analytical and numerical solution
-    ax1.plot(ts, xsn, '-x')
-    ax1.plot(ts, xsa, '-')
-    ax1.title.set_text('Temperature Benchmark 2')
-    ax1.set_ylabel("Temperature(degrees C)")
-    ax1.set_xlabel("Time (days)")
-
-    # Plotting the graph for absolute error term
-    ax2.plot(ts, np.absolute(xsn-xsa)/np.absolute(xsa), '-')
-    ax2.title.set_text('Error Analysis')
-    ax2.set_ylabel("Relative Error Against Benchmark")
-    ax2.set_xlabel("Time (days)")
-
-    # Plotting the graph for convergence testing
-    dtc = np.linspace(2, 16, 14)
-    lastval = 0*dtc
-    for i in range(14):
-        # Compute number of Euler steps to take
-        ndt = 1/dtc[i]
-        ntc = int(np.ceil((t1-t0)/ndt))
-        tc = t0+np.arange(ntc+1)*ndt
-        q1c = 0*tc
-        q1c.fill(10)
-        Pc = 15*q1c
-        tsc, xsnc = solve_ode_temp(
-            temp_ode_model, t0, t1, ndt, q1c, T0, Pc, pars2)
-        lastval[i] = xsnc[-1]
-
-    ax3.plot(dtc, lastval, 'o')
-    ax3.title.set_text('Timestep Convergence')
-    ax3.set_ylabel("X(t)")
-    ax3.set_xlabel("1/Δt")
-
-    plt.tight_layout()
-    plt.show()
-
-
 if __name__ == '__main__':
     #pressure_benchmark()
-    #temperature_benchmark1()
-    temperature_benchmark2()
+    temperature_benchmark()
